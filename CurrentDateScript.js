18c72c2d84bad363c27166c7ac739552cd546401
WhiteLightningGun <109026202+WhiteLightningGun@users.noreply.github.com>
2022-12-15 16:26:42 +0000

    Update CurrentDateScript.js

--- CurrentDateScript.js
+++ CurrentDateScript.js
@@ -4,7 +4,21 @@ function updateTime()
     var day = today.getDay();
     var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var minutes = today.getMinutes()
+
+    if (String(minutes).length == 1)
+    {
+       minutes = '0' + minutes
+    }
+
+    var seconds = today.getSeconds();
+
+    if (String(seconds).length == 1)
+    {
+       seconds = '0' + seconds
+    }
+
+    var time = today.getHours() + ":" + minutes + ":" + seconds;
     var dateTime = date+' '+time;
     document.getElementById("dateTime1").innerHTML = dateTime + ' <br>' + daylist[day];
 }
